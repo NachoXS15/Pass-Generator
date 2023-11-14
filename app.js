a4c3417a7fccc8b03c14d0731fc66a7acd636d9b
NachoXS-NET <pantojaignacio642@gmail.com>
2023-11-14 13:17:00 -0300

    created generator

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-const characters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
-
 const generatePassword = () => {
-    
+    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcedfghijklmnopqrstuvwxyz123456789$#.-_"
+    const length = 12;
+    let password = "";
+    for (let i = 0; i < length; i++) {
+        let index = Math.floor(Math.random() * characters.length);
+        password += characters.charAt(index);
+    }
+    console.log(password)
+    document.getElementById("passGen").innerText = password;
 }
