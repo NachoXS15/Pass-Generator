2bff22e11971443a51c437bacc23d49ac095ee07
NachoXS15 <86374911+NachoXS15@users.noreply.github.com>
2023-11-14 15:52:14 -0300

    created copy button

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const copyButton = document.getElementById("btnCopy")
+
 const generatePassword = () => {
     const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcedfghijklmnopqrstuvwxyz123456789$#.-_"
     const length = 12;
@@ -6,6 +8,18 @@ const generatePassword = () => {
         let index = Math.floor(Math.random() * characters.length);
         password += characters.charAt(index);
     }
-    console.log(password)
     document.getElementById("passGen").innerText = password;
+    copyButton.style.display = "block"
+}
+
+const copyPass = () => {
+    const copyText = document.getElementById("passGen")
+    const clipboard = navigator.clipboard;
+    clipboard.writeText(copyText.innerText)
+        .then(function() {
+            alert("copiado")
+        })
+        .catch(function(error){
+            console.error("no se copió", error)
+        })
 }
