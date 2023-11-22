d4c456dbd4ef743b3823fcb7f9ed6ce079fd3dbe
NachoXS15 <86374911+NachoXS15@users.noreply.github.com>
2023-11-22 10:56:26 -0300

    modified copy alert

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const copyButton = document.getElementById("btnCopy")
+const copyBtn = document.getElementById("btnCopy")
+const copyText = document.getElementById("passGen")
 
 const generatePassword = () => {
     const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcedfghijklmnopqrstuvwxyz123456789$#.-_"
@@ -10,15 +12,17 @@ const generatePassword = () => {
     }
     document.getElementById("passGen").innerText = password;
     copyButton.style.display = "block"
+    document.getElementById("generateBtn").innerText = "Generate Again"
+    copyBtn.innerText = "Copy"
 }
 
 const copyPass = () => {
-    const copyText = document.getElementById("passGen")
     const clipboard = navigator.clipboard;
     clipboard.writeText(copyText.innerText)
-        .then(function() {
-            alert("copiado")
-        })
+        .then(
+            copyBtn.innerText = "Copied!",
+            copyBtn.className = "btn btn-outline-light btn-lg mt-2"
+        )
         .catch(function(error){
             console.error("no se copió", error)
         })
